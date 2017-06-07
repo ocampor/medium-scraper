fcf2cb5dc62d03a59b4a52baefe06c7cdf8143f7
Ricardo Ocampo <ricardo.ocampo@wizeline.com>
2017-06-06 22:23:12 -0500

    Added class ArticleInfo that given a url extracts the body, title and tags of a medium article

---
+++ ArticleInfo.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class ArticleInfo:
+    def __init__(self, url):
+        self._driver = webdriver.PhantomJS()
+        self._url = url
+        self._initialize()
+
+    def _initialize(self):
+        self._driver.get(self._url)
+
+    @property
+    def body(self):
+        identifier = 'postArticle-content'
+        body = self._driver.find_element(By.CLASS_NAME, identifier)
+        return body.text
+
+    @property
+    def title(self):
+        return self._driver.title
+
+    @property
+    def tags(self):
+        identifier = 'ul.tags'
+        ul_tags = self._driver.find_element(By.CSS_SELECTOR, identifier)
+        tags = ul_tags.find_elements(By.TAG_NAME, 'li')
+        return [tag.text for tag in tags]
+
+    def to_json(self):
+        return {
+            'title': self.title,
+            'body': self.body,
+            'tags': self.tags
+        }
